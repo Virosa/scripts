28c245478d6607a7ad0218d17832bda40900b387
Virosa <dracopm@hotmail.com>
2016-05-12 16:49:04 -0400

    Delete unthrottle.sh

--- EthOS/unthrottle.sh
+++
@@ -1,3 +0,0 @@
-#Dirty Script by Virosa
-#!/bin/bash
-while [ 1 ]; do echo "Unthrottling..."; for i in `ls /var/run | grep throttle`; do echo -n "" > /var/run/$i; done && ethos-overclock; echo "...Sleeping for an hour."; sleep 3600; done;
