c32820e2634da9e477dce7a09abafa74f894ff9f
Virosa <dracopm@hotmail.com>
2016-05-12 15:50:28 -0400

    Rename startMineScript.sh to startMine/startMineScript.sh

---
+++ startMine/startMineScript.sh
@@ -0,0 +1,3 @@
+#Dirty Script by Virosa
+#!/bin/bash
+while [ 1 ]; do echo "Running Mine Start..."; mine start; echo "...Sleeping for 5 minutes."; sleep 300; done;
--- startMineScript.sh
+++
@@ -1,3 +0,0 @@
-#Dirty Script by Virosa
-#!/bin/bash
-while [ 1 ]; do echo "Running Mine Start..."; mine start; echo "...Sleeping for 5 minutes."; sleep 300; done;
