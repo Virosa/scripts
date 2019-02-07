347b9bc33abf0261191894db74fda5ec21f9c83d
Virosa <dracopm@hotmail.com>
2019-02-07 18:54:07 -0500

    Made a new branch instead

--- bminer-c31/bminer-c31.sh
+++ bminer-c31/bminer-c31.sh
@@ -4,12 +4,7 @@ cd /tmp/
 sudo rm miner-manager
 sudo wget https://raw.githubusercontent.com/virosa/third-party-miner-installer-for-ethos/master/miner-manager
 sudo chmod +x miner-manager
-sudo ./miner-manager bminer install
+sudo ./miner-manager bminer-c31 install
 disallow && minestop && sleep 1
-# lazy add C31 minerprocess.php
-sudo curl https://raw.githubusercontent.com/virosa/scripts/bminer-c31/minerprocess.php > /opt/ethos/lib/minerprocess.php
-sudo service ethos-miner-daemon restart 
-sudo service ethos-stats-daemon restart 
-sudo service ethos-gpu-stats-daemon restart
 sleep 1 && allow
 cd
