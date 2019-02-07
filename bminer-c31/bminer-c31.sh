d492f464cabe962be74ae8cacddaba9837863a3c
Virosa <dracopm@hotmail.com>
2019-02-07 17:59:31 -0500

    Create bminer-c31.sh

---
+++ bminer-c31/bminer-c31.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+disallow && minestop && sleep 1
+cd /tmp/
+sudo rm miner-manager
+sudo wget https://raw.githubusercontent.com/virosa/third-party-miner-installer-for-ethos/master/miner-manager
+sudo chmod +x miner-manager
+sudo ./miner-manager bminer install
+disallow && minestop && sleep 1
+# lazy add C31 minerprocess.php
+curl https://raw.githubusercontent.com/virosa/scripts/bminer-c31/minerprocess.php > /opt/ethos/lib/minerprocess.php
+sudo service ethos-miner-daemon restart 
+sudo service ethos-stats-daemon restart 
+sudo service ethos-gpu-stats-daemon restart
+sleep 1 && allow
+cd
